7dbac62c79d09582767cf6bbb4db17a3c4e70822
Raisa Guayasamin <raisa.marilyn1993@gmail.com>
2024-04-18 13:00:19 -0500

    Creacion de interfaz de modal y logica de crear y editar country

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './domain/countries/pages/list/list.component';
+import { CurrencyDetailComponent } from './domain/currencies/pages/currency-detail/currency-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+  {
+    path: 'currency',
+    component: CurrencyDetailComponent,
+  },
+];
